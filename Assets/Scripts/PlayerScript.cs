c0a7476a06f03964e0ff2875aaba455b4df1fbd9
Heikki Haapala <hhaapala@gmail.com>
2014-10-03 12:38:21 +0300

    rage + stats started

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,4 +2,16 @@
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
+
+    public float rage;
+
+    public float charisma;
+
+    public float talent;
+
+    public float teamplay;
+
+    public float reflex;
+
+    public float luck;
 }
